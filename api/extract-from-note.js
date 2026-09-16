ab60e5b6d71b5def2dfad734af5f05a0b0f38518
Whitney Williams <wwilliams@levelup-pd.com>
2026-09-15 21:50:27 -0400

    Fix talk pattern: allow 'and' between name and pronoun

--- api/extract-from-note.js
+++ api/extract-from-note.js
@@ -20,7 +20,7 @@ const commitmentPatterns = [
   // "he/she needs Y" with context
   /\b(?:he|she|they)\s+need(?:s|ed)?\s+(?:to\s+)?(.+?)(?:\.|$)/i,
   // "Talked to X, he needs Y"
-  /talk(?:ed|ing)?\s+(?:to|with)\s+([A-Z][a-z]+(?:\s+[A-Z][a-z]+)?)[,.]?\s+(?:he|she|they)\s+(?:need(?:s|ed)?|said|wants?)\s+(.+?)(?:\.|$)/i,
+  /talk(?:ed|ing)?\s+(?:to|with)\s+([A-Z][a-z]+(?:\s+[A-Z][a-z]+)?)[,.]?(?:\s+and\s+)?\s*(?:he|she|they)\s+(?:need(?:s|ed)?|said|wants?)\s+(.+?)(?:\.|$)/i,
   // "Remind me to Y"
   /remind\s+(?:me|us)\s+to\s+(.+?)(?:\.|$)/i,
   // "I need to Y by Z"
